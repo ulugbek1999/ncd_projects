f7dc84bcf5ad3b2c4b7ca49176274111bfb658fd
nulltyper <nulltyper@nulltypers-MacBook-Air.local>
2019-05-15 15:42:05 +0500

    new

--- src/components/unit1/GrammarDrag.vue
+++ src/components/unit1/GrammarDrag.vue
@@ -2,11 +2,22 @@
     <div style="position: absolute">
         <!-- HTML here -->
         <!--URL 127.0.0.1:8080/unit1/grammar/exercise -->
+        <p class="class">asd</p>        
+<p class="class">asd</p> 
+<p class="class">asd</p> 
+<p class="class">asd</p> 
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure officia animi non. Obcaecati, ipsa quam incidunt quo laborum facere a, fugiat magnam praesentium quia recusandae quas, molestiae harum vel atque.
+
     </div>
+
+
 </template>
 
 <style scoped>
     /* CSS styles are here */
+
+
+
 
 </style>
 
